c0af8d8da8702be019be46ee0cfdb4649a109762
Denis Ilic <ilic.denis@gmx.at>
2019-06-08 09:57:13 +0200

    CodeReview05

---
+++ Js/myscript.js
@@ -0,0 +1,44 @@
+var likes = 1;
+
+function like(){
+	document.getElementById("show").innerHTML = +likes;
+	likes = likes +1;
+}
+
+var likes1 = 1;
+
+function like1(){
+	document.getElementById("show1").innerHTML = +likes1;
+	likes1 = likes1 +1;
+}
+
+var likes2 = 0;
+
+function like2(){
+	document.getElementById("show2").innerHTML = +likes2;
+	likes2 = likes2 +1;
+}
+
+var likes3 = 0;
+
+function like3(){
+	document.getElementById("show3").innerHTML = +likes3;
+	likes3 = likes3 +1;
+}
+
+var likes4 = 0;
+
+function like4(){
+	document.getElementById("show4").innerHTML = +likes4;
+	likes4 = likes4 +1;
+}
+
+var likes5 = 0;
+
+function like5(){
+	document.getElementById("show5").innerHTML = +likes5;
+	likes5 = likes5 +1;
+}
+
+
+
