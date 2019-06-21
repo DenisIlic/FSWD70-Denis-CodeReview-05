1482ec538bd72807dfad9207de33ee723ad8cf5f
Denis Ilic <ilic.denis@gmx.at>
2019-06-22 00:57:25 +0200

    final

--- Js/myscript.js
+++ Js/myscript.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 			    <div class="col-lg-4 col-md-6 mx-auto "> 
 			        <img class="filmImg " src="${a[i].pic}" alt="${a[i].name}">
 			    </div>
-			        <div class="col-lg-8 ">
+			        <div class="col-lg-8 pl-5">
 			              
 			            <h1 class="movieTitle ml-lg-1 mx-md-auto text-white">${a[i].name}</h1>
 			            <p class="fdescrition filmImg mb-md-3 mb-sm-3 pl-1 text-white"> 
@@ -49,6 +49,11 @@ $(document).ready(function(){
 			                 	 <span class="likesNumber">${a[i].value}</span>
 			                  </span>
 			           </div>
+			           <div class="mt-md-5 mt-sm-5 text-danger">
+			                  <span class="i_dislike_it"><i class="fas fa-thumbs-down"></i> 
+			                 	 <span class="dislikesNumber">${a[i].value1}</span>
+			                  </span>
+			           </div>
 			        </div>
 			  </div>
 
@@ -58,6 +63,8 @@ $(document).ready(function(){
 
 		var like = $(".i_like_it");
 		var increasedLikes = $(".likesNumber");
+		var dislike = $(".i_dislike_it");
+		var decreasedLikes = $(".dislikesNumber");
 
 		for(let i = 0; i < film.length; i++){
 
@@ -77,12 +84,16 @@ $(document).ready(function(){
 
 			});
 
+					$(dislike[i]).on('click', function(){
+
+					--arrObj[i].value1;
+
+					$(decreasedLikes[i]).text(arrObj[i].value1);
+			});
+
 		}
 
-
-
 	}
-
 
 });
 
